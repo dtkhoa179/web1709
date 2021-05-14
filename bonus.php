61ca27340504b4775113618674d35cb2a4ae7bc8
dtkhoa179 <84089051+dtkhoa179@users.noreply.github.com>
2021-05-14 14:34:56 +0700

    Create bonus.php

---
+++ bonus.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
+</head>
+<body>
+
+<h1>Promotions</h1>
+
+<p>When shopping at our store, you will receive many promotions such as: vouchers, extremely attractive gifts....</p>
+
+</body>
+</html>
