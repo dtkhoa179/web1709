118c9206174a1e524ab10ae12058ed07a52674b7
dtkhoa179 <84089051+dtkhoa179@users.noreply.github.com>
2021-05-14 14:23:53 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,9 @@
-Welcome to the ATN toy store. ATN is a Vietnamese company which is selling toys to teenagers in many provinces all over Vietnam.
+<html>
+<body>
+
+<h3 style="color:Tomato;">Hi everybody</h3>
+
+<p style="color:DodgerBlue;">Welcome to the ATN toy store. ATN is a Vietnamese company which is selling toys to teenagers in many provinces all over Vietnam.</p>
+
+</body>
+</html>
