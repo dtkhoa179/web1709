602dfbb85895f26c287a71066da556944176b400
dtkhoa179 <84089051+dtkhoa179@users.noreply.github.com>
2021-05-14 14:25:50 +0700

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,8 @@
 
 <h3 style="color:Tomato;">Hi everybody</h3>
 
-<p style="color:DodgerBlue;">Welcome to the ATN toy store. ATN is a Vietnamese company which is selling toys to teenagers in many provinces all over Vietnam.</p>
+<p style="color:DodgerBlue;">Welcome to the ATN toy store. ATN is a Vietnamese company which is selling toys to teenagers in many provinces all over Vietnam. 
+  We are happy to serve you all.</p>
 
 </body>
 </html>
